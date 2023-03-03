2bd84f1c0d4e0d20f3f8b89d60438fc7a427d1cb
Venkata Sairam Yanamandra <venkatasairam.y@gmail.com>
2023-03-03 12:23:20 +0530

    updated post Router
    updated posts to perform delete/update operations only by the associated logged in user.

--- fast-api/app/routers/post.py
+++ fast-api/app/routers/post.py
@@ -44,6 +44,9 @@ def update_post(id: int, updated_post: schemas.PostCreate,  db: Session = Depend
     if post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} doesn't exists")
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="You're Not Authorized to perform the requested operation ")
 
     # post_query.update({"title": "This is my updated title",
         #                   "content": "updated content"}, synchronize_session=False)
@@ -57,12 +60,16 @@ def update_post(id: int, updated_post: schemas.PostCreate,  db: Session = Depend
 @router.delete("/{id}")
 def delete_user(id: int, db: Session = Depends(get_db), current_user=Depends(oauth2.get_current_user)):
 
-    result_data = db.query(models.Post).filter(models.Post.id == id)
-
-    if result_data.first() == None:
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post_result_set = post_query.first()
+    print(post_result_set)
+    if post_result_set == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="page not found with given id")
-    result_data.delete(synchronize_session=False)
+    if post_result_set.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail="You're Not Authorized to perform the requested operation ")
+    post_query.delete(synchronize_session=False)
     db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
