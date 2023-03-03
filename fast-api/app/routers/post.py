37bf3858d9ae65866ffb0ac2405181ea4da7883e
Venkata Sairam Yanamandra <venkatasairam.y@gmail.com>
2023-03-03 12:53:12 +0530

    updated query parameters

--- fast-api/app/routers/post.py
+++ fast-api/app/routers/post.py
@@ -8,8 +8,9 @@ router = APIRouter(prefix="/posts")
 
 
 @router.get("/")
-def get_all_posts(db: Session = Depends(get_db), response_model=schemas.PostAllResponses):
-    posts = db.query(models.Post).all()
+def get_all_posts(db: Session = Depends(get_db), response_model=schemas.PostAllResponses,  limitPosts: int = 5):
+
+    posts = db.query(models.Post).limit(limitPosts).all()
 
     return posts
 
